054c7ee8a7500c9aa6f80c11421d90d8bcb1054e
Hilora Dewett <h.dewett@outlook.com>
2022-12-09 16:41:52 -0500

    updated tweet form submission with ajax, created loadTweets to fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,52 +1,6 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-};
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function () {
   renderTweets(data);
 });
-
-const renderTweets = function (tweets) {
-  for (const item of tweets) {
-    const $tweet = createTweetElement(item);
-    $(`#tweets-container`).append($tweet)
-  }
-};
 
 const createTweetElement = function (tweetData) {
   const $tweet = $(`
@@ -79,3 +33,41 @@ const createTweetElement = function (tweetData) {
   return $tweet;
 
 };
+
+//Render each tweet
+const renderTweets = function (tweets) {
+  for (const item of tweets) {
+    const $tweet = createTweetElement(item);
+    $(`#tweets-container`).append($tweet)
+  }
+};
+
+// Fetching tweets from /tweets
+const loadTweets = function() {
+  const formData = ($(this).serialize());
+  $.ajax({
+    method: "GET",
+    url: "/tweets",
+    data: formData,
+    success: function(data) {
+      renderTweets(data);
+    }
+  })
+};
+loadTweets();
+
+// Submit the form
+$(".new-tweet form").submit(function(event) {  
+  event.preventDefault();
+  // Turns a set of form data into a query string
+  const formData = ($(this).serialize()); 
+
+  $.ajax({
+    type: "POST",
+    url: '/tweets',
+    data: formData,
+    success: function (data) {
+      loadTweets(data);
+    }
+  })
+});
